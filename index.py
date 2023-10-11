3cf0a1514ed0b2b399ad35ab183d77c7a26535bf
edu-correa <eduardo.correa@sptech.school>
2023-10-11 14:57:48 -0300

    ajeitei a conexao e um site disponivel

--- index.py
+++ index.py
@@ -1,12 +1,33 @@
 import requests
 import re
+import mysql.connector
 from bs4 import BeautifulSoup
+
 try:
-    resposta = requests.get("///")
-    site = BeautifulSoup(resposta.content, 'html.parser')
-    textoFoda = site.find('div', attrs={'data-target': 'processors-specifications'}).contents
-    #print(textoFoda)
-    potencia = re.findall("\d{1,2}\.\d{1,2} GHz", str(textoFoda[5]))
-    print(potencia)
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="urubu100",
+        password="urubu100",
+        database="BankSecure"
+    )
+    cr = mydb.cursor()
+    try:
+        resposta = requests.get("https://www.tempo.com/sao-paulo.htm")
+        site = BeautifulSoup(resposta.content, 'html.parser')
+        res = site.find('span', attrs={'class': 'dato-temperatura changeUnitT'}).contents
+        print(res[0])
+        valor = int((res[0].split("°"))[0])
+        
+        if valor > 26:
+            print("Alerta, temperatura do computador pode estar sendo influenciada pelo ambiente")
+            cr.execute("INSERT INTO alertas VALUES (null, 1, 'alerta altas temperaturas ambientes', 1, 1)")
+        else:
+            print("Qualquer temperatura alta é provinda do computador.")
+    except:
+        print("erro ao conectar ao banco de dados")
+
+        
 except:
     print("algo deu errado")
+
+
